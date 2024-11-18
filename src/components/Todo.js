7f9dcc51020402373d0ef5517f784120ecf7310b
dhia_raboudi <mohameddhia.raboudi@etudiant-enit.utm.tn>
2024-11-18 19:00:58 +0100

    Update to the latest version

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,31 +1,17 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+import './Todo.css';
 
-export const Todo = ({ task, toggleComplete }) => {
+export const Todo = ({ task, toggleComplete,deleteTodo,editTodo }) => {
   return (
     <div className='Todo'>
-      <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed' : ''}`}>
-        {task.task}
-      </p>
-      <div>
-        <FontAwesomeIcon icon={faPenToSquare} />
-        <FontAwesomeIcon icon={faTrash} />
+      <p onClick={() => toggleComplete(task.id)} className={`Todo-item-text ${task.completed ? 'completed' : ''}`}>{task.task}</p>
+      <div className='Todo-item-actions'>
+        <FontAwesomeIcon icon={faPenToSquare} onClick={()=> editTodo(task.id)  } className='icon edit-icon' />
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} className='icon delete-icon' />
       </div>
     </div>
   );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
